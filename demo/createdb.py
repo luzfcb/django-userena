561e0a3adf19ef0908f4c6d5e575730af2d8b8f7
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2014-03-16 23:15:22 -0300

    fixed imports on urls.py from demo project. fixed print function to compability to python3

--- demo/createdb.py
+++ demo/createdb.py
@@ -17,7 +17,7 @@ def create_dbs():
         db_type = db['ENGINE']
         # see if it is mysql
         if db_type.endswith('mysql'):
-            print 'creating database %s on %s' % (db_name, host)
+            print('creating database %s on %s' % (db_name, host))
             db = MySQLdb.connect(user=user,
                                 passwd=password,
                                 host=host,
@@ -37,15 +37,15 @@ def create_dbs():
                 print("database already exists, moving on to next step.")
         # see if it is postgresql
         elif db_type.endswith('postgresql_psycopg2'):
-            print 'creating database %s on %s' % (db_name, host)
+            print('creating database %s on %s' % (db_name, host))
             con = psycopg2.connect(host=host, user=user, password=password, port=port, database='postgres')
             con.set_isolation_level(0)
             cur = con.cursor()
             try:
                 cur.execute('CREATE DATABASE %s' % db_name)
             except psycopg2.ProgrammingError as detail:
-                print detail
-                print 'moving right along...'
+                print(detail)
+                print('moving right along...')
         else:
             print("ERROR: {0} is not supported by this script, you will need to create your database by hand.".format(db_type))
 
